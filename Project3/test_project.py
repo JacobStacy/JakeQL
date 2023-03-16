d493d568311499dd6d04eef543f5df30e917165d
Jacob Robert Stacy <35983856+JacobStacy@users.noreply.github.com>
2023-03-16 14:34:40 -0400

    working on project 3

---
+++ Project3/test_project.py
@@ -0,0 +1,37 @@
+import project
+import sqlite3
+import os
+
+database = "test1.db"
+sql_file = "self.test.sql"
+sql_file = "test.update.01.sql"
+
+try:
+    os.remove(database)
+except:
+    pass
+
+result = ""
+conn = project.connect(database)
+
+with open(sql_file, 'r') as file:
+    for line in file.readlines():
+        result = conn.execute(line)
+        
+
+expected = ""
+conn = sqlite3.connect(database)
+
+with open(sql_file, 'r') as file:
+    for line in file.readlines():
+        expected = conn.execute(line)
+    expected = expected.fetchall()
+    
+
+# os.remove(database)
+
+
+print("expected:",  expected)
+print("student: ",  result)
+print(str(expected) == str(result))
+# assert str(expected) == str(result)
